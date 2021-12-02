998d154065e00eedceff4268615439817c671393
Daniele <danieledaniele1988@gmail.com>
2021-12-02 23:31:24 +0100

    added Timer

--- components/GameBoard.js
+++ components/GameBoard.js
@@ -1,13 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import React from "react";
+import React,{useEffect, useState} from "react";
 import {StyleSheet, Text, View} from "react-native";
 import Square from './Square';
 
 const GameBoard = () => {
+  const [timeLeft, setTimeLeft] = useState(10)
+
+  useEffect(() => {
+    if(!timeLeft) return
+    const timerID = setInterval(() => {
+      // happens every second
+      setTimeLeft(timeLeft -1)
+    },1000)
+      return() => clearInterval(timerID)
+  }, [timeLeft])
+
+
+
     return (
         <View style={styles.container}>
-          {/*<Text>Open up App.js to start working on your app!</Text>*/}
+          <Text>My first App!</Text>
           <StatusBar style="auto" />
+          <Text>{timeLeft}</Text>
           <View style={styles.game}>
             <Square></Square>
             <Square></Square>
