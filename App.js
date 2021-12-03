351b1720ca34b410ba92282694ebb341ca236ea7
Daniele <danieledaniele1988@gmail.com>
2021-12-03 10:23:59 +0100

    Autosave

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import GameBoard from './components/GameBoard'
+import { Provider } from 'react-redux'
 
 export default function App() {
   return(
-    <GameBoard></GameBoard>
+    <Provider>
+      <GameBoard></GameBoard>
+    </Provider>
   )
 }
 
